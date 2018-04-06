71c8f37c96201a288a5272a2c81b4e5940b3e1e8
Dave Wongillies <dave.gillies@gmail.com>
2018-04-06 13:08:13 -0700

    Add rlwrap to figlet

--- figlet/entrypoint.sh
+++ figlet/entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
+clear
 
-if [ -z "$*" ]; then
-    while :; do
-        read figlet
-        figlet -tk "${figlet}"
-    done
-else
-    figlet "$*"
-fi
+# 1 second sleep due docker issue https://github.com/moby/moby/issues/10341
+sleep 1
+
+rlwrap figlet
